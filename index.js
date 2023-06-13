5828bfb5a848274006f87f43a4222a017567de90
DenDev82 <dendev82@gmail.com>
2023-06-13 09:23:09 +0200

    ISUS

---
+++ index.js
@@ -0,0 +1,27 @@
+//function sabiranje(a, b){
+//    return a+b;
+//}
+//function oduzimanje (a,b){
+//    return a-b;
+//}
+//function mnozenje(a,b){
+//    return a*b;
+//}
+//function dijeljenje(a,b){
+//    return a/b;
+//}
+//
+//var a = parseInt(prompt(), 10);
+//var b = parseInt(prompt() ,10);
+//
+//function ispis(){
+//    console.log(`Zbir je ${sabiranje(a,b)}`);
+//    console.log(oduzimanje(a,b));
+//    console.log(mnozenje(a,b));
+//    console.log(dijeljenje(a,b));
+//}
+//
+//ispis();
+//
+//console.log(`${a} * ${b} = ${a*b}`)
+//
